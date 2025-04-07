fc7548b6eb819b0d7d10a05179e0bf03526508e4
Vandan Vandan <vaish>
2025-04-06 20:40:07 -0500

    Removing File

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCK3NPedsUv0AeeSOWjnkIUlT01OIycSkI")
-
-# List available models (this should show if gemini-pro is supported in v1beta)
-models = genai.list_models()
-for model in models:
-    print(model.name, model.supported_generation_methods)
